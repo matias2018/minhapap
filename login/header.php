0fe25acdbc1e86d9641e67036e5e2e382f11e7bc
garatugeiro <46558651+garatugeiro@users.noreply.github.com>
2019-01-10 10:02:41 +0000

    Add files via upload

---
+++ login/header.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<nav> 
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<form>
+						<input type="text" name="iud" placeholder="Username/e-mail">
+						<input type="password" name="pwd" placeholder="password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">Sign up</a>
+				</div>
+			</div>
+		</nav>
+
+    </header>
---
+++ login/includes/dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbServername ="localhost";
+$dbUsername ="root";
+$dbPassword ="";
+$dbName ="login2018";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+/*44 how to create a complete login system in php (read desc) php tutorial learn php programming*/
