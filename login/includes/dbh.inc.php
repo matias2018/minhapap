7d07fa80ee6b820b2e2d22d97fdebd27db0fcf1f
matias2018 <36554761+matias2018@users.noreply.github.com>
2019-01-14 12:26:40 +0000

    connect or die
    $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);

--- login/includes/dbh.inc.php
+++ login/includes/dbh.inc.php
@@ -6,6 +6,9 @@ $dbPassword ="";
 $dbName ="login2018";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
 
 
 /*44 how to create a complete login system in php (read desc) php tutorial learn php programming*/
+
+
